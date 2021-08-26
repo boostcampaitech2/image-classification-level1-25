df10f29529e1559f8831ede104d0d705253e387a
leedaehee <vckfnv@naver.com>
2021-08-26 04:38:57 +0000

    daehee

---
+++ models/model_kj copy.py
@@ -0,0 +1,33 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+
+class resnetbase(nn.Module):
+    def __init__(self, num_classes: int = 1000):
+        super().__init__()
+        self.superM = torchvision.models.resnet18(pretrained=True)
+    
+        self.superM.fc = torch.nn.Linear(in_features=512, out_features=num_classes, bias=True)
+        torch.nn.init.xavier_uniform_(self.superM.fc.weight)
+        stdv = 1/np.sqrt(512)
+        self.superM.fc.bias.data.uniform_(-stdv, stdv)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.superM(x)
+        return x
+    
+ 
+class resnetbase2(nn.Module):
+    def __init__(self, num_classes: int = 1000):
+        super().__init__()
+        self.superM = torchvision.models.resnet18(pretrained=True)
+    
+        self.superM.fc = torch.nn.Linear(in_features=512, out_features=num_classes, bias=True)
+        torch.nn.init.xavier_uniform_(self.superM.fc.weight)
+        stdv = 1/np.sqrt(512)
+        self.superM.fc.bias.data.uniform_(-stdv, stdv)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.superM(x)
+        return x
