7cbbbd7a2dcd4481de7949653a8baa9b009ebfaa
hojihun5516 <hojihun5516@daum.net>
2021-08-26 14:37:22 +0000

    delete: delete checkpoint files

--- models/model_jh.py
+++ models/model_jh.py
@@ -3,27 +3,12 @@ import torch
 import torch.nn as nn
 import torchvision
 
-class resnetbase(nn.Module):
-    def __init__(self, num_classes: int = 1000):
+class vggnetbase(nn.Module):
+    def __init__(self, num_classes: int = 18):
         super().__init__()
-        self.superM = torchvision.models.resnet18(pretrained=True)
+        self.superM = torchvision.models.vgg16(pretrained=True)
     
-        self.superM.fc = torch.nn.Linear(in_features=512, out_features=num_classes, bias=True)
-        torch.nn.init.xavier_uniform_(self.superM.fc.weight)
-        stdv = 1/np.sqrt(512)
-        self.superM.fc.bias.data.uniform_(-stdv, stdv)
-        
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.superM(x)
-        return x
-    
- 
-class resnetbase2(nn.Module):
-    def __init__(self, num_classes: int = 1000):
-        super().__init__()
-        self.superM = torchvision.models.resnet18(pretrained=True)
-    
-        self.superM.fc = torch.nn.Linear(in_features=512, out_features=num_classes, bias=True)
+        self.superM.fc = torch.nn.Linear(in_features=512, out_features=18, bias=True)
         torch.nn.init.xavier_uniform_(self.superM.fc.weight)
         stdv = 1/np.sqrt(512)
         self.superM.fc.bias.data.uniform_(-stdv, stdv)
