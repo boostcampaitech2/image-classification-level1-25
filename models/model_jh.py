fae78ae88282210c9374152cd00d81a640a5d031
hojihun5516 <hojihun5516@daum.net>
2021-08-27 02:40:49 +0000

    feat: add vggnet19 model

--- models/model_jh.py
+++ models/model_jh.py
@@ -6,13 +6,18 @@ import torchvision
 class vggnetbase(nn.Module):
     def __init__(self, num_classes: int = 18):
         super().__init__()
-        self.superM = torchvision.models.vgg16(pretrained=True)
+        self.superM = torchvision.models.vgg19(pretrained=True)
     
-        self.superM.fc = torch.nn.Linear(in_features=512, out_features=18, bias=True)
-        torch.nn.init.xavier_uniform_(self.superM.fc.weight)
-        stdv = 1/np.sqrt(512)
-        self.superM.fc.bias.data.uniform_(-stdv, stdv)
-        
+        self.superM.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.superM(x)
         return x
