7a79a72be7198bdd6d7746c7434ba48d3010fcf7
err <ppskj178@gmail.com>
2021-08-28 15:41:06 +0000

    add uncomplete dataset

--- datasets/dataset_kj.py
+++ datasets/dataset_kj.py
@@ -198,3 +198,70 @@ class basicDatasetA(Dataset):
 
     def __len__(self):
         return len(self.df_csv)*7
+
+
+def set_gender(gender):
+    return 0 if gender == 'male' else 1
+
+def set_age(age):
+    if age<30:
+        return 0
+    elif age<60:
+        return 1
+    else:
+        return 2
+
+class EvalTestDatasetA(Dataset): #미완성
+    def __init__(self, data_path, train = 'ALL', transform = None, N = 0):
+        self.main_path = data_path
+        self.transform = transform
+        df_origin = pd.read_csv(os.path.join(self.main_path, 'train.csv'))
+        print(df_origin['age'][0])
+        print(set_age(df_origin['age'][0]))
+        print('-'*50)
+        df_origin['gender_age_cls'] = df_origin.apply(lambda x : set_age(x['age']) + 3*set_gender(x['gender']) ,axis=1)
+
+        from sklearn.model_selection import train_test_splitx
+        train_df, val_df = train_test_split(df_origin, test_size=150, stratify=df_origin.gender_age_cls, random_state=52)
+
+        train_df, fold1 = train_test_split(df_origin, test_size=20%, stratify=df_origin.gender_age_cls, random_state=52)
+        train_df, fold2 = train_test_split(train_df, test_size=25%, stratify=df_origin.gender_age_cls, random_state=52)
+        train_df, fold3 = train_test_split(train_df, test_size=33%, stratify=df_origin.gender_age_cls, random_state=52)
+        fold5, fold4 = train_test_split(train_df, test_size=50%, stratify=df_origin.gender_age_cls, random_state=52)
+
+        from sklearn.model_selection import StratifiedKFold
+        train_ids, val_ids = [], []
+        for x, y in StratifiedKFold().split():
+            train_ids.append(x)
+            val_ids.append(y)
+
+
+
+
+        total = len(df_origin)
+        if train == 'ALL':
+            self.df_csv = df_origin
+        elif train == 'train' :
+            self.df_csv = train_df
+        elif train == 'test' :
+            self.df_csv = val_df
+
+        elif train == 'fold' : 
+            if state == 'train':
+                self.df_csv = train_df[train_ids[fold_idx]]
+            elif state == 'vaild':
+                self.df_csv = train_df[val_ids[fold_idx]]
+
+        else :
+            raise Exception(f'train error {train} not in [''ALL'', ''train'', ''test'']')
+        
+
+
+
+
+    def __getitem__(self, index):
+        # return self.df_csv.iloc[index]
+        return self.df_csv
+
+    def __len__(self):
+        return len(self.df_csv)*7
