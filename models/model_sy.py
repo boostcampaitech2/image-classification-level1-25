91c29dfabe0229b03528a2cbd63def0e3429f4fe
SuhYoon <euniejeon@snu.ac.kr>
2021-08-29 18:03:03 +0000

    feat : face recognition

--- models/model_sy.py
+++ models/model_sy.py
@@ -1,48 +1,24 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torchvision
-import numpy as np 
-
-class vgg19base(nn.Module):
-    def __init__(self, num_classes :int = 1000):
-        super().__init__()
-        self.model = torchvision.models.vgg19_bn(pretrained = True)
-        self.model.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(True),
-            nn.Dropout(),
-            nn.Linear(4096, num_classes),
-        )
-        initialize_weights(self.model.classifier)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.model(x)
-        return x
-
-
-    
-def initialize_weights(model):
-    """
-    Initialize all weights using xavier uniform. 
-    For more weight initialization methods, check https://pytorch.org/docs/stable/nn.init.html
-    """
-    for m in model.modules():
-        if isinstance(m, nn.Conv2d):
-            init.xavier_uniform_(m.weight.data)
-            if m.bias is not None:
-                m.bias.data.zero_()
-        elif isinstance(m, nn.BatchNorm2d):
-            m.weight.data.fill_(1)
-            m.bias.data.zero_()
-        elif isinstance(m, nn.Linear):
-            m.weight.data.normal_(0, 0.01)
-            m.bias.data.zero_()
-
 
 class resnetbase(nn.Module):
+    def __init__(self, num_classes: int = 1000):
+        super().__init__()
+        self.superM = torchvision.models.resnet18(pretrained=True)
+    
+        self.superM.fc = torch.nn.Linear(in_features=512, out_features=num_classes, bias=True)
+        torch.nn.init.xavier_uniform_(self.superM.fc.weight)
+        stdv = 1/np.sqrt(512)
+        self.superM.fc.bias.data.uniform_(-stdv, stdv)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.superM(x)
+        return x
+    
+ 
+class resnetbase2(nn.Module):
     def __init__(self, num_classes: int = 1000):
         super().__init__()
         self.superM = torchvision.models.resnet18(pretrained=True)
