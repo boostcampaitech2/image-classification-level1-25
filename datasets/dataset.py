320a063259396491537c4ece5aa20baebd07f5df
SuhYoon <euniejeon@snu.ac.kr>
2021-08-30 12:49:52 +0000

    merge : branch 'master' into suhyoon

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -248,19 +248,37 @@ class MaskSplitByProfileDataset(MaskBaseDataset):
 
 
 class TestDataset(Dataset):
-    def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
+    def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), transfrom = None):
         self.img_paths = img_paths
-        self.transform = transforms.Compose([
-            Resize(resize, Image.BILINEAR),
-            ToTensor(),
-            Normalize(mean=mean, std=std),
-        ])
+        if transfrom:
+            self.transform = transfrom
+        else:
+            self.transform = transforms.Compose([
+                Resize(resize, Image.BILINEAR),
+                ToTensor(),
+                Normalize(mean=mean, std=std),
+            ])
 
     def __getitem__(self, index):
         image = Image.open(self.img_paths[index])
 
         if self.transform:
             image = self.transform(image)
+        return image
+
+    def __len__(self):
+        return len(self.img_paths)
+
+class TestDatasetA(Dataset):
+    def __init__(self, img_paths, resize, mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246), transfrom = None):
+        self.img_paths = img_paths
+        self.transform = transfrom
+
+    def __getitem__(self, index):
+        image = Image.open(self.img_paths[index])
+
+        if self.transform:
+            image = self.transform(image=np.array(image))['image'].float()
         return image
 
     def __len__(self):
@@ -384,6 +402,79 @@ class teamDataset(Dataset):
         else : raise Exception(f'파일명 오류 : {file_path}, {files[sub_index]}, {mask}')
 
         return image, y#, age #age는 사진 수동 검증용, 나이 파악이 힘듬
+
+    def __len__(self):
+        return len(self.df_csv)*7
+
+
+class MMteamDataset(Dataset):
+    num_classes = {'mask' : 3, 'gender' : 2, 'age' : 3, 'merged' : 3*2*3, 'concat': 3+2+3}
+    def __init__(self, data_dir, mode = 'ALL', transform = None, val_ratio = 0.1):
+        self.main_path = data_dir
+        self.transform = transform
+        self.mode = mode
+        df_origin = pd.read_csv(os.path.join(self.main_path, 'train.csv'))
+        df_origin['gender'] = df_origin['gender'].map({'male':0, 'female':1})
+        df_origin['gender_age_cls'] = df_origin.apply(lambda x : set_age(x['age']) + 3*set_gender(x['gender']) ,axis=1)
+        train_df, eval_df = train_test_split(df_origin, test_size=150, stratify=df_origin.gender_age_cls, random_state=25)
+
+        train_df = train_df.sample(frac=1).reset_index(drop=True)
+
+        train_share = 1 - val_ratio
+        total = len(train_df)
+        if mode == 'ALL':
+            self.df_csv = train_df
+        elif mode == 'train' :
+            self.df_csv = train_df.head(int(total*train_share)) 
+        elif mode == 'valid' :
+            self.df_csv = train_df.tail(total-int(total*train_share))
+        else :
+            raise Exception(f'train error {mode} not in [''ALL'', ''train'', ''test'']')
+    
+    def set_transform(self, transform):
+        """
+        transform 함수를 설정하는 함수입니다.
+        """
+        self.transform = transform
+
+    def __getitem__(self, index):
+        main_index, sub_index = index//7, index%7
+        sub_path = self.df_csv.iloc[main_index]['path']
+        file_path = os.path.join(self.main_path, 'images', sub_path)
+        files = [file_name for file_name in os.listdir(file_path) if file_name[0] != '.']
+
+        image = Image.open(os.path.join(file_path, files[sub_index]))
+
+        if self.transform:
+            image = self.transform(image=np.array(image))['image'].float()
+        
+        labed_dict = {}
+        y = 0
+
+        if (age := self.df_csv.iloc[main_index]['age']) < 30:
+            labed_dict['age'] = 0
+        elif age >= 30 and age < 60:
+            y += 1
+            labed_dict['age'] = 1
+        else :
+            y += 2
+            labed_dict['age'] = 2
+
+        y += self.df_csv.iloc[main_index]['gender'] * 3
+        labed_dict['gender'] = self.df_csv.iloc[main_index]['gender']
+
+        if (mask := files[sub_index][0]) == 'm' :
+            labed_dict['mask'] = 0
+        elif mask == 'i' :
+            y += 6
+            labed_dict['mask'] = 1
+        elif mask == 'n' :
+            y += 12
+            labed_dict['mask'] = 2
+        else : raise Exception(f'파일명 오류 : {file_path}, {files[sub_index]}, {mask}')
+        
+        labed_dict['merged'] = y
+        return image, labed_dict
 
     def __len__(self):
         return len(self.df_csv)*7
--- models/model.py
+++ models/model.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
+import timm
 
 
 class BaseModel(nn.Module):
@@ -68,4 +69,49 @@ class resnetbase(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.superM(x)
         return x
+
+
+class rexnet_200base(nn.Module):
+    def __init__(self, num_classes: int = 1000):
+        super().__init__()
+        self.superM = timm.create_model(model_name = "rexnet_200", # 불러올 모델 architecture,
+                                        num_classes=num_classes, # 예측 해야하는 class 수
+                                        pretrained = True # 사전학습된 weight 불러오기
+                                    )
     
+        # self.superM.fc = torch.nn.Linear(in_features=512, out_features=num_classes, bias=True)
+        # torch.nn.init.xavier_uniform_(self.superM.fc.weight)
+        # stdv = 1/np.sqrt(512)
+        # self.superM.fc.bias.data.uniform_(-stdv, stdv)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.superM(x)
+        return x
+    
+    
+class MultiModelMergeModel(nn.Module):
+    def __init__(self, modelMASK, modelAGE, modelGENDER,
+                    concatclasses : int = 8 , num_classes: int = 18,
+                    prev_model_frz=True ):
+        super().__init__()
+        self.prev_model_frz = prev_model_frz
+
+        self.modelMASK = modelMASK
+        self.modelAGE = modelAGE
+        self.modelGENDER = modelGENDER
+        self.classifier = nn.Sequential(
+            nn.Linear(concatclasses, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, num_classes),
+        )
+        
+    def forward(self, IMAGE):
+        MASK = self.modelMASK(IMAGE)
+        AGE = self.modelAGE(IMAGE)
+        GENDER = self.modelGENDER(IMAGE)
+        if self.prev_model_frz :
+            MERGED = torch.cat((MASK.detach(), AGE.detach(), GENDER.detach()), dim=1)
+        else :
+            MERGED = torch.cat((MASK, AGE, GENDER), dim=1)
+        MERGED = self.classifier(nn.functional.relu(MERGED))
+        return MERGED
