ab7c7fa40cbd37a8e2532c711b9227f8de32caeb
SuhYoon <euniejeon@snu.ac.kr>
2021-08-30 06:35:52 +0000

    chore : dataset_sy, model_sy, trans_sy modified

--- trans/trans_sy.py
+++ trans/trans_sy.py
@@ -1,59 +1,43 @@
-from torchvision import transforms
-import albumentations as A
-import albumentations.pytorch as Ap
+from albumentations import *
+from albumentations.pytorch import ToTensorV2
 
-def basic_train_trans():
-    return transforms.Compose([
-                            # transforms.CenterCrop(300),
-                            transforms.ToTensor(),
-                            # transforms.Normalize(0.5, 0.5),
-                            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
-                            ])
 
-def basic_test_trans():
-    return transforms.Compose([
-                        # transforms.CenterCrop(300),
-                        transforms.ToTensor(),
-                        # transforms.Normalize(0.5, 0.5),
-                        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.2, 0.2, 0.2)),
-                        ])
+def get_transforms(need=('train', 'val'), img_size=(512, 384), mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
+    transformations = {}
+    if 'train' in need:
+        transformations['train'] = Compose([
+            Resize(img_size[0], img_size[1], p=1.0),
+            HorizontalFlip(p=0.5),
+            ShiftScaleRotate(p=0.5),
+            HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
+            RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=0.5),
+            GaussNoise(p=0.5),
+            Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
+            ToTensorV2(p=1.0),
+        ], p=1.0)
 
-def A_just_tensor():
-    return A.Compose([
-                        Ap.ToTensorV2()
-                    ])
+    if 'val' in need:
+        transformations['val'] = Compose([
+            Resize(img_size[0], img_size[1]),
+            Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
+            ToTensorV2(p=1.0),
+        ], p=1.0)
+        
+def basic_train_trans(img_size=(512, 384), mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
+    return Compose([
+            Resize(img_size[0], img_size[1], p=1.0),
+            HorizontalFlip(p=0.5),
+            ShiftScaleRotate(p=0.5),
+            HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.5),
+            RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=0.5),
+            GaussNoise(p=0.5),
+            Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
+            ToTensorV2(p=1.0),
+        ], p=1.0)
 
-def A_random_trans():
-    return A.Compose([
-                        A.CenterCrop(height=384, width=384),
-                        A.OneOf([
-                            A.RandomCrop(height=256, width=256),
-                            A.Resize(height=256, width=256),
-                        ], p=1.0),
-                        A.HorizontalFlip(p=0.5),
-                        A.ShiftScaleRotate(p=0.7),
-                        A.RandomBrightnessContrast(brightness_limit=(-0.3, 0.3), contrast_limit=(-0.3, 0.3), p=0.5),
-                        A.GaussNoise(var_limit=(1000, 1600), p=0.2),
-                        A.OneOf([
-                            A.GridDropout(),
-                            A.GlassBlur(),
-                            A.GaussianBlur(),
-                            A.ColorJitter(),
-                            A.Equalize(),
-                            A.ChannelDropout(),
-                            A.ChannelShuffle(),
-                        ]),
-                        Ap.ToTensorV2()
-                    ])
-
-def A_simple_trans():
-    return A.Compose([
-                        A.CenterCrop(height=384, width=384),
-                        A.OneOf([
-                            A.RandomCrop(height=256, width=256),
-                            A.Resize(height=256, width=256),
-                        ], p=1.0),
-                        A.HorizontalFlip(p=0.5),
-                        A.ShiftScaleRotate(p=0.7),
-                        Ap.ToTensorV2()
-                    ])
+def basic_test_trans(img_size=(512, 384), mean=(0.548, 0.504, 0.479), std=(0.237, 0.247, 0.246)):
+    return Compose([
+            Resize(img_size[0], img_size[1]),
+            Normalize(mean=mean, std=std, max_pixel_value=255.0, p=1.0),
+            ToTensorV2(p=1.0),
+        ], p=1.0)
