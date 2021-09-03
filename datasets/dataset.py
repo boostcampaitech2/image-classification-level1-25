556915a953fcfc912970b0e52705749dde8afe31
err <ppskj178@gmail.com>
2021-09-03 05:54:30 +0000

    final 5

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -301,6 +301,64 @@ class basicDatasetA(Dataset):
     
     def __init__(self, data_dir, mode = 'ALL', transform = None):
         self.main_path = data_dir
+        self.transform = transform
+        self.mode = mode
+        df_origin = pd.read_csv(os.path.join(self.main_path, 'train.csv'))
+        df_origin['gender'] = df_origin['gender'].map({'male':0, 'female':1})
+        df_origin['gender_age_cls'] = df_origin.apply(lambda x : set_age(x['age']) + 3*x['gender'] ,axis=1)
+
+        train_df, eval_df = train_test_split(df_origin, test_size=150, stratify=df_origin.gender_age_cls, random_state=25)
+        train_df.reset_index(drop=True, inplace=True)
+        eval_df.reset_index(drop=True, inplace=True)
+
+        if mode == 'ALL':
+            self.df_csv = df_origin
+        elif mode == 'train' :
+            self.df_csv = train_df
+        elif mode == 'eval' :
+            self.df_csv = eval_df
+        else :
+            raise Exception(f'train error {mode} not in [ALL, train, eval]')
+        
+    def set_transform(self, transform):
+        self.transform = transform
+
+    def __getitem__(self, index):
+        main_index, sub_index = index//7, index%7
+        sub_path = self.df_csv.iloc[main_index]['path']
+        file_path = os.path.join(self.main_path, 'images', sub_path)
+        files = [file_name for file_name in os.listdir(file_path) if file_name[0] != '.']
+
+        image = Image.open(os.path.join(file_path, files[sub_index]))
+
+        if self.transform:
+            image = self.transform(image=np.array(image))['image'].float()
+
+        y = 0
+
+        if (age := self.df_csv.iloc[main_index]['age']) < 30: pass
+        elif age >= 30 and age < 60: y += 1
+        else : y += 2
+
+        y += self.df_csv.iloc[main_index]['gender'] * 3
+
+        if (mask := files[sub_index][0]) == 'm' : pass
+        elif mask == 'i' : y += 6
+        elif mask == 'n' : y += 12
+        else : raise Exception(f'파일명 오류 : {file_path}, {files[sub_index]}, {mask}')
+
+        return image, y#, age #age는 사진 수동 검증용, 나이 파악이 힘듬
+
+    def __len__(self):
+        return len(self.df_csv)*7
+
+
+# double data
+class basicDatasetA2(Dataset):
+    num_classes = 3 * 2 * 3
+    
+    def __init__(self, data_dir, mode = 'ALL', transform = None):
+        self.main_path = data_dir
         self.origin_trans = A_centercrop_trans()
         self.transform = transform
         self.mode = mode
